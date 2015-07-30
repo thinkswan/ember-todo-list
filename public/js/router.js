4cb9df7edf6c795d3231a401272c13adf7062c23
Graham Swan <thinkswan@gmail.com>
2015-07-30 21:12:02 +0700

    Add route to show completed todos

--- public/js/router.js
+++ public/js/router.js
@@ -1,6 +1,7 @@
 Todos.Router.map(function () {
   this.resource('todos', { path: '/' }, function () {
     this.route('active');
+    this.route('completed');
   });
 });
 
@@ -14,6 +15,18 @@ Todos.TodosActiveRoute = Ember.Route.extend({
   model: function() {
     return this.store.filter('todo', function(todo) {
       return !todo.get('isCompleted');
+    });
+  },
+
+  renderTemplate: function(controller) {
+    this.render('todos/index', { controller: controller });
+  }
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return todo.get('isCompleted');
     });
   },
 
