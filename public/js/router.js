ab72f626512d2b0e4b46bb059cd5ac6120899604
Graham Swan <thinkswan@gmail.com>
2015-07-30 21:07:44 +0700

    Move todo list into separate template

--- public/js/router.js
+++ public/js/router.js
@@ -1,5 +1,13 @@
-Todos.Router.map(function() {
-  this.resource('todos', { path: '/' });
+Todos.Router.map(function () {
+  this.resource('todos', { path: '/' }, function () {
+    // Additional child routes go here
+  });
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
+  model: function() {
+    return this.modelFor('todos');
+  }
 });
 
 Todos.TodosRoute = Ember.Route.extend({
