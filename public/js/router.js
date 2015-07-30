3c9243060a5cd9381dc0d7b3b21c236a0df4c1b7
Graham Swan <thinkswan@gmail.com>
2015-07-30 21:10:34 +0700

    Add route to show incomplete todos

--- public/js/router.js
+++ public/js/router.js
@@ -1,12 +1,24 @@
 Todos.Router.map(function () {
   this.resource('todos', { path: '/' }, function () {
-    // Additional child routes go here
+    this.route('active');
   });
 });
 
 Todos.TodosIndexRoute = Ember.Route.extend({
   model: function() {
     return this.modelFor('todos');
+  }
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+  model: function() {
+    return this.store.filter('todo', function(todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+
+  renderTemplate: function(controller) {
+    this.render('todos/index', { controller: controller });
   }
 });
 
